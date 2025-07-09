af4eba75e5bd956c240616d543233df76a567446
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2025-07-10 03:27:26 +0600

    Part-1 - 1.0

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express, { Application, Request, Response } from "express";
-
+import cors from "cors"
+import { userRoutes } from "./app/modules/users/user.routes";
 
 const app:Application = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/v1/user", userRoutes)
 
 app.get("/", (req:Request, res:Response)=>{
     res.send("Here is he Tour managment Backend")
