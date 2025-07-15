34a844d4afba62bed8754746abe11f7368523bac
Rasel Shikder <rasel.sikder777.rk@gmail.com>
2025-07-15 19:15:15 +0000

    Added mongoose duplicate, Cast and validation error in global error - 2.0

--- src/app/middleware/globalError.ts
+++ src/app/middleware/globalError.ts
@@ -1,25 +1,63 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from "express"
-import { envVars } from "../config/env"
-import appError from "../errorHelper/appError"
+import { NextFunction, Request, Response } from "express";
+import { envVars } from "../config/env";
+import appError from "../errorHelper/appError";
 
-export const globalError = (err:any, req:Request, res:Response, next:NextFunction)=>{
-    let statusCode = 500
-    let message = `Somthing went wrong in ${err.message} from global error`
+/**Duplicate error Mongoose */
 
-    if (err instanceof appError) {
-        statusCode = err.statusCode
-        message = err.message
-    } else if(err instanceof Error){
-        statusCode = 501
-        message = err.message
-    }
+/**Mongoose cast Error */
 
-    res.status(statusCode).json({
-        success:false,
-        message,
-        err,
-        stack: envVars.NODE_ENV === "Development" ? err.stack : null
+/** Mongoose validation error */
+
+const errorsSource:any = []
+
+export const globalError = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let statusCode = 500;
+  let message = `Somthing went wrong in ${err.message} from global error`;
+
+  // Cast Error / Object id error
+  if (err.name === "CastError") {
+    statusCode = 400;
+    message = "Invalid Mongodb Object Id, Provide a valid id";
+  }
+  // Mongoose validation error
+  else if (err.name === "ValidationError") {
+    statusCode = 400;
+    const errors = Object.values(err.errors)
+    errors.forEach((errorObject:any)=>{
+      errorsSource.push({
+        path:errorObject.path,
+        message:errorObject.message
+      })
     })
-}
+    message = `Validation Error`;
+  }
+  // Duplicate Error
+  else if (err.code === 1000) {
+    // eslint-disable-next-line no-console
+    console.log("Duplicate error");
+    const matchedArray = err.message.match(/"([^"]*)"/);
+    statusCode = 400;
+    message = `${matchedArray[1]} already exists`;
+  } else if (err instanceof appError) {
+    statusCode = err.statusCode;
+    message = err.message;
+  } else if (err instanceof Error) {
+    statusCode = 501;
+    message = err.message;
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorsSource,
+    // err,
+    stack: envVars.NODE_ENV === "Development" ? err.stack : null,
+  });
+};
