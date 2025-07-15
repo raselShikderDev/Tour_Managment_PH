52ae61270174be4eb391b1deb7ffcf4eb8462d8c
Rasel Shikder <rasel.sikder777.rk@gmail.com>
2025-07-15 22:53:21 +0000

    Put each bussiness logic of error in helper function to organize

--- src/app/middleware/globalError.ts
+++ src/app/middleware/globalError.ts
@@ -3,20 +3,80 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
 import appError from "../errorHelper/appError";
+import mongoose from "mongoose";
 
+interface TErrorSource {
+  path: string;
+  message: string;
+}
 
+interface TResponse {
+  statusCode: number;
+  message: string;
+  errorsSource: TErrorSource[];
+}
 
-/**Mongoose error can appear as error 
+let errorsSource: TErrorSource[] = [];
+
+const handleCastError = (err: mongoose.CastError): { statusCode: number; message: string } => {
+  return {
+    statusCode: 400,
+    message: "Invalid Mongodb Object Id, Provide a valid id",
+  };
+};
+
+const handleValidationError = (err: any): TResponse => {
+  const errorsSource: any = [];
+  const errors = Object.values(err.errors);
+
+  errors.forEach((errorObject: any) => {
+    errorsSource.push({
+      path: errorObject.path,
+      message: errorObject.message,
+    });
+  });
+  return {
+    statusCode: 400,
+    message: `Validation Error`,
+    errorsSource,
+  };
+};
+
+const handleDuplicateError = (err: any): TResponse => {
+  const errorsSource: any = [];
+  const matchedArray = err.message.match(/"([^"]*)"/);
+  const message = `${matchedArray[1]} already exists`;
+  return {
+    statusCode: 400,
+    message,
+    errorsSource,
+  };
+};
+
+const handleZodError = (err: any): TResponse => {
+  const issues = err.issues;
+  issues.forEach((issue: any) => {
+    errorsSource.push({
+      path: `${issue.path[issue.path.length - 1]} ${issue.code}`,
+      message: issue.message,
+    });
+  });
+
+  return {
+    statusCode: 400,
+    message: `Zod Error`,
+    errorsSource,
+  };
+};
+
+/**Mongoose error can appear as error
  * 1. cast Error
  * 2. Duplicate error
- * 3. validation error 
+ * 3. validation error
  */
 
 /** Zod Error can appear as error
- * 1.
  */
-
-const errorsSource:any = []
 
 export const globalError = (
   err: any,
@@ -25,46 +85,35 @@ export const globalError = (
   next: NextFunction
 ) => {
   let statusCode = 500;
-  let message = `Somthing went wrong in ${err.message} from global error`;
+  let message = `Somthing went wrong: ${err.message}`;
 
   // Cast Error / Object id error
   if (err.name === "CastError") {
-    statusCode = 400;
-    message = "Invalid Mongodb Object Id, Provide a valid id";
-  }
-  // Mongoose validation error
-  else if (err.name === "ValidationError") {
-    statusCode = 400;
-    const errors = Object.values(err.errors)
-    errors.forEach((errorObject:any)=>{
-      errorsSource.push({
-        path:errorObject.path,
-        message:errorObject.message
-      })
-    })
-    message = `Validation Error`;
+    const simplifyError = handleCastError(err);
+    statusCode = simplifyError.statusCode;
+    message = simplifyError.message;
   }
   // Duplicate Error
   else if (err.code === 1000) {
-    // eslint-disable-next-line no-console
-    console.log("Duplicate error");
-    const matchedArray = err.message.match(/"([^"]*)"/);
-    statusCode = 400;
-    message = `${matchedArray[1]} already exists`;
+    const simplifyError = handleDuplicateError(err);
+    statusCode = simplifyError.statusCode;
+    message = simplifyError.message;
+    errorsSource = simplifyError.errorsSource;
+  }
+  // Mongoose validation error
+  else if (err.name === "ValidationError") {
+    const simplifyError = handleValidationError(err);
+    statusCode = simplifyError.statusCode;
+    message = simplifyError.message;
+    errorsSource = simplifyError.errorsSource;
   }
   // Zod Error
-  else if (err.name === "ZodError"){
-    statusCode = 400
-    const issues = err.issues
-    issues.forEach((issue:any)=>{
-      errorsSource.push({
-        path:`${issue.path[issue.path.length - 1]} ${issue.code}` ,
-        message:issue.message,
-      })
-    })
-    message = `Zod Error`
-  }
-   else if (err instanceof appError) {
+  else if (err.name === "ZodError") {
+    const simplifyError = handleZodError(err);
+    statusCode = simplifyError.statusCode;
+    message = simplifyError.message;
+    errorsSource = simplifyError.errorsSource;
+  } else if (err instanceof appError) {
     statusCode = err.statusCode;
     message = err.message;
   } else if (err instanceof Error) {
@@ -76,7 +125,7 @@ export const globalError = (
     success: false,
     message,
     errorsSource,
-    // err,
+    err: envVars.NODE_ENV === "Development" ? err.stack : null,
     stack: envVars.NODE_ENV === "Development" ? err.stack : null,
   });
 };
