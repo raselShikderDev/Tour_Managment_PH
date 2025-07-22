43de9d7141e7e59f0166569c4bd31c809192b66b
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2025-07-22 20:26:07 +0600

    Added Booking and payment interfaces

---
+++ src/app/modules/booking/booking.interface.ts
@@ -0,0 +1,19 @@
+// User -> Booking (Pending) -> Payment (unpaid) -> SSLCommerz -> Booking status chnage to Confrim -> Payment status chnage to paid ->
+
+import { Types } from "mongoose";
+
+
+enum BOOKING_STATUS {
+    PENDING = "PENDING",
+    COMPLETED = "COMPLETED",
+    FAILD ="FAILD",
+    CANCELED = "CANCELED"
+}
+
+export interface Ibooking{
+    user:Types.ObjectId,
+    tour:Types.ObjectId,
+    payment:Types.ObjectId,
+    guestCount:number,
+    status: BOOKING_STATUS,
+}
